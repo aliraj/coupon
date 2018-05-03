f31d3bd6d7157bdbb6121fbcdd1f48472f979b56
lht_ge@163.com <is860125>
2018-05-03 14:48:48 +0800

    客服

--- coupon-entity/src/main/java/com/roof/coupon/accesslog/entity/AccessLogVo.java
+++ coupon-entity/src/main/java/com/roof/coupon/accesslog/entity/AccessLogVo.java
@@ -1,31 +1,43 @@
 package com.roof.coupon.accesslog.entity;
 
+import com.roof.coupon.itemcoupon.entity.ItemCoupon;
+import com.roof.coupon.itemcoupon.entity.ItemCouponVo;
+
 import java.util.List;
 
 /**
  * @author 模版生成 <br/>
- *         表名： c_access_log <br/>
- *         描述：用户行为日志 <br/>
+ * 表名： c_access_log <br/>
+ * 描述：用户行为日志 <br/>
  */
 public class AccessLogVo extends AccessLog {
 
-	private List<AccessLogVo> accessLogList;
+    private List<AccessLogVo> accessLogList;
 
-	public AccessLogVo() {
-		super();
-	}
+    private ItemCouponVo itemCouponVo;
 
-	public AccessLogVo(Long id) {
-		super();
-		this.id = id;
-	}
+    public AccessLogVo() {
+        super();
+    }
 
-	public List<AccessLogVo> getAccessLogList() {
-		return accessLogList;
-	}
+    public AccessLogVo(Long id) {
+        super();
+        this.id = id;
+    }
 
-	public void setAccessLogList(List<AccessLogVo> accessLogList) {
-		this.accessLogList = accessLogList;
-	}
+    public List<AccessLogVo> getAccessLogList() {
+        return accessLogList;
+    }
 
+    public void setAccessLogList(List<AccessLogVo> accessLogList) {
+        this.accessLogList = accessLogList;
+    }
+
+    public ItemCouponVo getItemCouponVo() {
+        return itemCouponVo;
+    }
+
+    public void setItemCouponVo(ItemCouponVo itemCouponVo) {
+        this.itemCouponVo = itemCouponVo;
+    }
 }
